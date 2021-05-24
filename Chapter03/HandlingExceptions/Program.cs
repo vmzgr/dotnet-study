94e0524c54ee03ab2b56b1302f0666793f072de4
vmzgr <freearchitect@gmail.com>
2021-05-24 15:01:56 +0300

    second commit

---
+++ Chapter03/HandlingExceptions/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+namespace HandlingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello, C#!");
+            // test git behavior
+            // test2
+        }
+    }
+}
